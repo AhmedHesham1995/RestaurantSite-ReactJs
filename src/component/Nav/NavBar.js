620f8067fe7e5fa5c3f40db3aca62fdc93a5d717
AhmedHesham1995 <ahmed.hesham.ahmed10@gmail.com>
2023-06-22 19:18:12 +0300

    last

--- src/component/Nav/NavBar.js
+++ src/component/Nav/NavBar.js
@@ -6,21 +6,26 @@ import { NavLink } from 'react-router-dom'
 
 
 function NavBar(){
-
+    const handleNavClick=()=>{
+        window.scrollTo({
+            top:0,
+            behavior:'smooth'
+        })
+    }
     return(
         <Navbar fixed='top' expand="lg">
             <Container>
-                <Navbar.Brand href="/">
+                <Navbar.Brand href="/" onClick={handleNavClick}>
                     <img src={logo} alt='logo'/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/about">About Us</NavLink>
-                    <NavLink to="/explore">Explore Foods</NavLink>
-                    <NavLink to="/review">Review</NavLink>
-                    <NavLink to="/faq">FAQ</NavLink>
+                    <NavLink to="/" onClick={handleNavClick}>Home</NavLink>
+                    <NavLink to="/about" onClick={handleNavClick}>About Us</NavLink>
+                    <NavLink to="/explore" onClick={handleNavClick}>Explore Foods</NavLink>
+                    <NavLink to="/review" onClick={handleNavClick}>Review</NavLink>
+                    <NavLink to="/faq" onClick={handleNavClick}>FAQ</NavLink>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
